cfe57797b1d463678c818e5b0ce502a32434faf7
gchu <gchu647@gmail.com>
2018-07-31 17:12:11 -1000

    white boarding NF

--- denormal_cars.sql
+++ denormal_cars.sql
@@ -1,21 +1,22 @@
 -- CREATE USER denormal_user;
 -- CREATE DATABASE denormal_cars OWNER denormal_user;
 
--- 5)
--- SELECT DISTINCT make_title 
+-- 5) There are (71) different makes(brand) of cars
+-- SELECT DISTINCT make_title
 -- FROM car_models;
 
--- 6)
+-- 6) Just the volks brand alone there is (27) models
 -- SELECT DISTINCT model_title 
 -- FROM car_models 
 -- WHERE make_code='VOLKS'; 
 
--- 7)
+-- 7) There are (136) different cars made by 'LAM'
+-- a lot of repetitive cars because it was separated by different year 
 -- SELECT DISTINCT make_code, model_code, model_title, year 
 -- FROM car_models 
 -- WHERE make_code='LAM';
 
--- 8)
-SELECT DISTINCT car_models
+-- 8) listing car models between 2010 and 2015
+SELECT DISTINCT *
 FROM car_models
 WHERE year BETWEEN 2010 AND 2015;
---
+++ normal_cars.sql
@@ -0,0 +1,6 @@
+--Table 1 There are 1314 cars with distinct make_title and model_title
+-- SELECT DISTINCT make_title, model_title
+-- FROM car_models
+-- ORDER BY 
+--   make_title ASC,
+--   model_title ASC;
