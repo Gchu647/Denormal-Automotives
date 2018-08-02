ae4c8b5d9d0edc7d8402e9e7b96f38bfaf039c7a
gchu <gchu647@gmail.com>
2018-08-01 20:42:58 -1000

    working put make_id colummn and model_id column in

--- normal_cars.sql
+++ normal_cars.sql
@@ -1,48 +1,41 @@
 -- First Table (71) unqiue makes
 
--- CREATE TABLE make_id_table (
---   make_id SERIAL PRIMARY KEY,
---   make_code VARCHAR(50) NOT NULL,
---   make_title VARCHAR(50) NOT NULL
--- );
+CREATE TABLE make (
+  id SERIAL PRIMARY KEY,
+  make_code VARCHAR(50) NOT NULL,
+  make_title VARCHAR(50) NOT NULL
+);
 
--- INSERT INTO make_id_table(make_code, make_title)
--- SELECT DISTINCT make_code, make_title
--- FROM car_models
--- ORDER BY 
---   make_code ASC;
+INSERT INTO make (make_code, make_title)
+SELECT DISTINCT make_code, make_title
+FROM car_models
+ORDER BY 
+  make_code ASC;
 
 -- Second Table (1314) unqie models
 
--- CREATE TABLE model_id_table (
---   model_id SERIAL PRIMARY KEY,
---   model_code VARCHAR(50) NOT NULL,
---   model_title VARCHAR(50) NOT NULL
--- );
+CREATE TABLE model (
+  id SERIAL PRIMARY KEY,
+  model_code VARCHAR(50) NOT NULL,
+  model_title VARCHAR(50) NOT NULL
+);
 
--- INSERT INTO model_id_table(model_code, model_title)
--- SELECT DISTINCT model_code, model_title
--- FROM car_models
--- ORDER BY 
---   model_code ASC;
+INSERT INTO model(model_code, model_title)
+SELECT DISTINCT model_code, model_title
+FROM car_models
+ORDER BY 
+  model_code ASC;
 
-
-
--- Third Table an id that includes make_id and model id
-
--- CREATE TABLE car_models_id (
---   id SERIAL PRIMARY KEY,
---   year INTEGER NOT NULL,
---   make_id REFERENCES make_id_table(make_id);
---   model_id REFERENCES model_id_table(model_id);
--- );
+-- Adding an id into car_models
+ALTER TABLE car_models
+  ADD COLUMN id serial PRIMARY KEY;
 
 
--- Working on getting the data and inserting it in
+-- The data to insert it in
 -- INSERT INTO car_models_id(year, make_id, model_id)
-SELECT DISTINCT car_models.year, make_id_table.make_id, model_id_table.model_id
+SELECT DISTINCT car_models.year, make.make_code, model.model_code, make.id, model.id
   FROM car_models
-  INNER JOIN make_id_table ON car_models.make_title = make_id_table.make_title
-  INNER JOIN model_id_table ON car_models.model_title = model_id_table.model_title
+  INNER JOIN make ON car_models.make_code = make.make_code
+  INNER JOIN model ON car_models.model_title = model.model_title
 ORDER BY 
   year ASC;
